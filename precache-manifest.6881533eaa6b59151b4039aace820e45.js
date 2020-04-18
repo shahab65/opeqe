28f77174cb288215bb8086a1b85cd2a78cc4df7f
shahab <shahab.emami95@gmail.com>
2020-04-18 21:42:49 +0430

    Updates

---
+++ precache-manifest.6881533eaa6b59151b4039aace820e45.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bad58fefa330d8454b74f31444c4d4a",
+    "url": "/opeqe/index.html"
+  },
+  {
+    "revision": "9ae09dff9d6abaa7f6e6",
+    "url": "/opeqe/static/css/main.eb946ca5.chunk.css"
+  },
+  {
+    "revision": "8e02420d00fac842c175",
+    "url": "/opeqe/static/js/2.d1f8f160.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/opeqe/static/js/2.d1f8f160.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ae09dff9d6abaa7f6e6",
+    "url": "/opeqe/static/js/main.23979552.chunk.js"
+  },
+  {
+    "revision": "2d02c5d07281ab948d56",
+    "url": "/opeqe/static/js/runtime-main.b8c07800.js"
+  },
+  {
+    "revision": "91f42da8a4c73cd24c48d12795fde403",
+    "url": "/opeqe/static/media/android_mobile.91f42da8.jpg"
+  },
+  {
+    "revision": "e374ae720b14326ea930e037a7086df6",
+    "url": "/opeqe/static/media/app-store.e374ae72.svg"
+  },
+  {
+    "revision": "d4396cb23ddbdaca1ae29a1d04748a32",
+    "url": "/opeqe/static/media/bg.d4396cb2.jpg"
+  },
+  {
+    "revision": "4b2cc4bb33daddb7aa09cfbfa5146703",
+    "url": "/opeqe/static/media/full_mobile.4b2cc4bb.jpg"
+  },
+  {
+    "revision": "13399d0a15a564723a772926964cb4d1",
+    "url": "/opeqe/static/media/gift_card.13399d0a.jpg"
+  },
+  {
+    "revision": "c68e1c2faea7c511f7f5093e1997520f",
+    "url": "/opeqe/static/media/google-play.c68e1c2f.svg"
+  },
+  {
+    "revision": "82550d23a526251d85ddb6f672071c0b",
+    "url": "/opeqe/static/media/ios_mobile.82550d23.jpg"
+  },
+  {
+    "revision": "31ec7f4a365ba997623a48561faff1cb",
+    "url": "/opeqe/static/media/sushi.31ec7f4a.jpg"
+  }
+]);
